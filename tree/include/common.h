89f3035eb16861ea2cb9762e4b518530909b6ba7
cuijin <lingkou@yscredit.com>
2019-07-30 23:37:58 +0800

    hug clion

---
+++ tree/include/common.h
@@ -0,0 +1,14 @@
+//
+// Created by 崔进 on 2019-07-17.
+//
+
+#ifndef LEETCODE_C_COMMON_H
+#define LEETCODE_C_COMMON_H
+
+struct TreeNode {
+    int val;
+    struct TreeNode *left;
+    struct TreeNode *right;
+};
+
+#endif //LEETCODE_C_COMMON_H
